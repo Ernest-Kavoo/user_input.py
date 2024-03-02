e1f35815b9ff7b8fa30fe5b8a9e9d7246cc058a4
Ernest-Kavoo <161935064+Ernest-Kavoo@users.noreply.github.com>
2024-03-02 09:05:51 -0800

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,15 @@
+# user_input.py
+
+# user's name
+name = input("what is your name?")
+
+# user's age 
+age = input("what is your age?")
+
+# user's location
+location = input("what is your location?")
+
+# print personalized message
+print("hello{} you are{} year's old and you live in {}".foramt(name, age, location))
+
+
